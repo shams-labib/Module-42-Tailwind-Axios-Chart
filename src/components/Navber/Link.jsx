f90dfd94785bdbfdac58427b4e21e4196ed275a9
shams al labib <samslabib19@gmail.com>
2025-09-27 23:10:04 +0600

    added

--- src/components/Navber/Link.jsx
+++ src/components/Navber/Link.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = ({route}) => {
     return (
         <div>
- 
+               
               <li className='mr-5'><a href={route.path}>{route.name}</a></li>
   
         </div>
--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 // Example Navbar Data
 const navbar1 = [
   { id: 1, name: "Home", path: "/" },
@@ -11,27 +12,34 @@ const navbar1 = [
 
 
 const Navber = () => {
-    return (
-         <nav>
+  // so ekhane ekta bisoy ache, jeta amar mathay thake na and ami eta vul kori seta holo, ekta truthy value suppose "open" er samne jdi "!open" evabe di, tahole man ta toggle korbe, mane true thakle false korbe abar false thakle man ta true korbe, so eta ekta havvy jinish ei jinish ta mathay rakha dorkar ache
 
-<ul className='flex'>
+  const [open, setOpen] = useState(false)
+
+  const links =  navbar1.map(route => <Link route={route}></Link>)
+
+
+    return (
+         <nav className='flex justify-between mx-10'>
+            <span className='flex' onClick={()=> setOpen(!open)}>  
+              {open?<X className='md:hidden'></X>:<Menu className='md:hidden'></Menu>}
+
+              <ul className='md:hidden'>
+                {links}
+              </ul>
+              
+            <h1 className='mr-3'>My Navber</h1>
+            </span>
+
+<ul className='md:flex hidden'>
     
       {
-        navbar1.map(route => <Link route={route}></Link>)
+       links
       }
 </ul>
 
+ <button className='btn'>Sign Up</button>
             
-      {/* <ul className='flex'>
-
-     {
-      navbar1.map(route => 
-        <li className='text-white mr-4'><a href={route.path}>{route.name}</a></li>
-       )
-     }
-  
-
-      </ul> */}
      </nav>
     );
 };
